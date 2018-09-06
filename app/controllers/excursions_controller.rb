ba9ee85bf65bc42135b74cd37e3995303c7c951d
Courtney <admin@Courtneys-Macbook-Air-2.local>
2018-09-06 14:37:52 -0400

    add excursions to passenger

--- app/controllers/excursions_controller.rb
+++ app/controllers/excursions_controller.rb
@@ -3,13 +3,15 @@ class ExcursionsController < ApplicationController
   def new
     if params[:passenger_id]
       @excursion = Excursion.new
+      @passenger = Passenger.find(params[:passenger_id])
+      @excursions = Excursion.all
     else
       redirect_to root_path
     end
   end
 
   def create
-    @excursion = Excursion.find(params[:excursion_id])
+    @excursion = Excursion.find(params[:excursion][:id])
     @passenger = Passenger.find(session[:passenger_id])
     @passenger.excursions << @excursion
     @passenger.save
@@ -17,7 +19,8 @@ class ExcursionsController < ApplicationController
   end
 
   def show
-
+    @excursion = Excursion.find(params[:id])
+    @passenger = Passenger.find(params[:passenger_id])
   end
 
 
