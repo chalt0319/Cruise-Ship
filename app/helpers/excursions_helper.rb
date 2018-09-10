b494c91ae5e1f97a3faf7ee9296ab004e3837bf3
Courtney <admin@Courtneys-Macbook-Air-2.local>
2018-09-10 16:37:09 -0400

    pluralize hours for excursions

--- app/helpers/excursions_helper.rb
+++ app/helpers/excursions_helper.rb
@@ -1,2 +1,6 @@
 module ExcursionsHelper
+
+  def excursion_pluralize(excursion)
+    pluralize(excursion.duration, "hour")
+  end
 end
--- app/helpers/passengers_helper.rb
+++ app/helpers/passengers_helper.rb
@@ -4,4 +4,8 @@ module PassengersHelper
     @captain = Captain.find_by(id: passenger.ship.captain_id)
     @captain.name
   end
+
+  def pluralize_hours(passenger)
+    pluralize(passenger.total_excursion_time, "hour")
+  end
 end
