98a58661b86d1c2acedf902354737c3cfda3906e
Courtney <admin@Courtneys-Macbook-Air-2.local>
2018-09-10 11:03:39 -0400

    header and css added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
 
+  
+  helper_method :current_user
+  helper_method :logged_in?
+  helper_method :current_user_path
+
+
+  def welcome
+    render "layouts/welcome"
+  end
+
   def check_current_user
     if @passenger
       if @passenger.id == session[:passenger_id]
@@ -16,7 +26,26 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def welcome
-    render "layouts/welcome"
+  def logged_in?
+    if !!session[:passenger_id]
+      true
+    else
+      false
+    end
   end
+
+  def current_user
+    if logged_in?
+      @passenger = Passenger.find(session[:passenger_id])
+      "#{@passenger.name}'s Profile"
+    else
+      false
+    end
+  end
+
+  def current_user_path
+    @passenger = Passenger.find(session[:passenger_id])
+    user_path(@passenger)
+  end
+
 end
