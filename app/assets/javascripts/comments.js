6c08a9a2cbf2bd54f1b65e0bea305cd2e7a70f1f
Courtney <chalt0319@gmail.com>
2018-10-11 15:33:45 -0400

    Code cleanup

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -2,24 +2,17 @@ function addComment(passenger, excursion, pe) {
  let url = `/passengers/${passenger}/excursions/${excursion}/passenger_excursions/${pe}/edit`
  $.get(url, function (response) {
    $(".id_" + excursion).append(response)
-   // $(".add_comment_form").submit(function (e) {
-   //   e.preventDefault()
-   //   submitComment(passenger, excursion, pe)
-   // })
  })
 }
 
 
 function submitComment(passenger, excursion, pe) {
   let url = `/passengers/${passenger}/excursions/${excursion}/passenger_excursions/${pe}`
-  // let url = this.action
-  // debugger
   let excursionId = excursion
   let data = {passenger: passenger, excursion: excursion, comment: $(`.comment_text_area_${excursionId}`).last().val()}
   let posting = $.post(url, data)
 
   posting.done(function (info) {
-    // debugger
     $(".edit_passenger_excursion").hide()
     $(".id_" + info.excursion_id + " .add_comment").text("Edit Comment")
     $(".commentList_" + excursionId).text("")
